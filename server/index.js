14ad00ea506967e67cc3d088840ad5a48c65b8b7
elliot <elliotgcraig@icloud.com>
2019-02-01 10:01:14 -0700

    set up most of boiler plate

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const massive = require('massive');
+require('dotenv').config();
+const ctrl = require('./controller');
+
+const app = express();
+app.use(bodyParser.json());
+
+massive(process.env.CONNECTION_STRING).then((db)=>{
+    app.set('db', db)
+}).catch((err)=>{console.log(err)})
+
+//app.get and stuff here
+
+serverPort = process.env.SERVER_PORT || 4000
+app.listen(serverPort, ()=>{console.log(`listening on the port ${serverPort}`)})
---
+++ src/ducks/reducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+
+}
+
+//type var goes here
+
+//payload functions go here
+
+export default function reducer(state = initialState, action){
+    switch(action.type){
+        //case
+        default:
+            return state
+    }
+}
